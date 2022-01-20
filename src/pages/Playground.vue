62cd4c6028f45048e3b04693f484a6818c42784a
davbrican <72500941+davbrican@users.noreply.github.com>
2022-01-20 19:26:00 +0100

    fix: blur div at the end of the session

--- src/pages/Playground.vue
+++ src/pages/Playground.vue
@@ -184,7 +184,7 @@
       </div>
     </div>
     <div
-      v-if="starting"
+      v-if="starting && (standardSession == true && testCounter != 3 || standardSession == false)"
       class="fixed h-full w-full top-0 z-50 flex justify-center items-center"
       style="backdrop-filter: blur(2px);"
     >
@@ -197,7 +197,6 @@
     >
       <div
         class="border-teal-600 p-8 border-t-8 bg-white mb-6 rounded-md shadow-lg m-5 w-2/3"
-        v-if="standardSession == true && testCounter != 3 || standardSession == false"
       >
         <h1 class="font-bold text-2xl mb-4">A new test begins!</h1>
         <h2 class="font-bold text-xl text-gray-600">
