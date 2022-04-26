e51b85aa4b5a70ef97d7ee01e3800c2a6f8a896c
davbrican <72500941+davbrican@users.noreply.github.com>
2022-04-26 18:21:39 +0200

    Update Lobby.vue

--- src/pages/Lobby.vue
+++ src/pages/Lobby.vue
@@ -147,7 +147,7 @@ export default {
       console.log(JSON.stringify(val.user));
       localStorage.setItem("user", JSON.stringify(val.user));
       localStorage.setItem("pairedTo", val.pairedTo);
-      localStorage.pairSocketId = JSON.stringify(val.pairSocketId);
+      localStorage.setItem("pairSocketId", "77918");
     },
     clientJoinAgain() {
       this.performConnection();
