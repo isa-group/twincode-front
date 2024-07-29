4b1a1412d4c4f466c3b65aa2d7efb2756d3924fb
rafgoncas1 <rafaelgonzalezcastillero@gmail.com>
2024-07-29 20:33:28 +0200

    Fix modal component
    Co-authored-by: PABLO PEREZ GASPAR <pabpergas@alum.us.es>

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,5 +1,6 @@
 <template>
   <div
+    :id="modalId"
     class="modal opacity-0 pointer-events-none fixed w-full h-full top-0 left-0 flex items-center justify-center"
   >
     <div
@@ -9,24 +10,15 @@
     <div
       class="modal-container bg-white w-11/12 md:max-w-xl mx-auto rounded shadow-lg z-50 overflow-y-auto"
     >
-      <!--<div
-        class="modal-close absolute top-0 right-0 cursor-pointer flex flex-col items-center mt-4 mr-4 text-white text-sm z-50"
-      >
-        <svg
-          class="fill-current text-white"
-          xmlns="http://www.w3.org/2000/svg"
-          width="18"
-          height="18"
-          viewBox="0 0 18 18"
-        >
-          <path
-            d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"
-          ></path>
+      <!-- Modal Close Button (Commented out) -->
+      <!-- <div class="modal-close absolute top-0 right-0 cursor-pointer flex flex-col items-center mt-4 mr-4 text-white text-sm z-50">
+        <svg class="fill-current text-white" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
+          <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
         </svg>
         <span class="text-sm">(Esc)</span>
-      </div>-->
+      </div> -->
 
-      <!-- Add margin if you want to see some of the overlay behind the modal-->
+      <!-- Modal Content -->
       <div class="modal-content py-4 text-left px-6">
         <!--Title-->
         <div class="flex justify-between items-center pb-3">
@@ -82,10 +74,14 @@ export default {
       default: "Cancel",
     },
   },
+  beforeCreate() {
+    // Generate a unique ID for the modal
+    this.modalId = 'modal-' + Math.random().toString(36).substr(2, 9);
+  },
   methods: {
     toggleModal() {
       const body = document.querySelector("body");
-      const modal = document.querySelector(".modal");
+      const modal = document.getElementById(this.modalId);
       modal.classList.toggle("opacity-0");
       modal.classList.toggle("pointer-events-none");
       body.classList.toggle("modal-active");
